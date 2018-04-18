0f61a425dc792851504346dea3fe39ab00d9fd7a
itggot-edvin-ketabati-augustinsson <edvin.ketabatiaugustinsson@itggot.se>
2018-04-18 13:26:10 +0200

    Misunderstood the assignment

--- lib/between.rb
+++ lib/between.rb
@@ -1,7 +1,7 @@
 def between(num1, num2, num3)
-    if num1 >= num3
+    if num1 > num3
         return false
-    elsif num2 >= num1
+    elsif num2 > num1
         return false
     else
         return true
