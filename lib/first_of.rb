3de78b6b8e84bb6ba19c6eb88dd0e2d985787411
itggot-edvin-ketabati-augustinsson <edvin.ketabatiaugustinsson@itggot.se>
2018-04-25 14:06:46 +0200

    Made last_of and first_of functions + started doing documentation

---
+++ lib/first_of.rb
@@ -0,0 +1,11 @@
+# array  - The array that you seek the first element of.
+#
+# Examples
+#
+#   first_of([0,1,3,5])
+#   # => 0
+#
+# Returns the first element of the array
+def first_of(array)
+    return array[0]
+end
---
+++ lib/last_of.rb
@@ -0,0 +1,11 @@
+# array  - The array that you seek the last element of.
+#
+# Examples
+#
+#   last_of([0,1,3,5])
+#   # => 5
+#
+# Returns the last element of the array
+def last_of(array)
+    return array[-1]
+end
